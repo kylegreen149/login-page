902cf9f7892ef2216e22a9a982d2024cd51130e9
Kyle Greenidge <kylegreen149@gmail.com>
2025-06-11 17:14:11 -0400

    Implement Login and Signup components with validation and state management

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Login = () => {
-    return <h2>Login Page</h2>
+    const [formData, setFormData] = useState({"username": "", "password": ""})
+    const isDisabled = !formData.username || !formData.password
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name] : e.target.value})
+        // console.log(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Attempted login with:", formData)
+    }
+
+    return (
+        <>
+            <h2>Login Page</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Username
+                    <input type="text" name="username" placeholder="Enter username" value={formData.username} onChange={handleChange}/>
+                </label>
+                <label>Password
+                    <input type="password" name="password" placeholder="Enter password" value={formData.password} onChange={handleChange}/>
+                </label>
+                <button type="submit" disabled={isDisabled}>Login</button>
+            </form>
+        </>
+    )
 }
+
+// Step 1. Create basic JSX (skeleton)
+// Step 2. Import useState and incorporate logic
 
 export default Login
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Signup = () => {
-    return <h2>Sign-up Page</h2>
+    const [formData, setFormData] = useState({"email": "", "username": "", "password": ""})
+    const isDisabled = !formData.email || !formData.username || !formData.password
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name] : e.target.value})
+        // console.log(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Successfully created account with:", formData)
+    }
+
+    return (
+        <>
+            <h2>Sign-up Page</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Email
+                    <input type="email" name="email" placeholder="Enter email" value={formData.email} onChange={handleChange}/>
+                </label>
+                <label>Username
+                    <input type="text" name="username" placeholder="Enter username" value={formData.username} onChange={handleChange}/>
+                </label>
+                <label>Password
+                    <input type="password" name="password" placeholder="Enter password" value={formData.password} onChange={handleChange}/>
+                </label>
+                <button type="submit" disabled={isDisabled}>Sign Up</button>
+            </form>
+        </>
+
+    )
 }
 
 export default Signup
